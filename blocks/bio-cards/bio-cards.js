c59dc66964981512600471fd0b5f6bd6849eb84c
Snehankita Osaramkar <you@example.com>
2026-02-21 14:58:20 +0530

    bio card block created

---
+++ blocks/bio-cards/bio-cards.js
@@ -0,0 +1,7 @@
+// blocks/bio-cards/bio-cards.js
+
+export default function decorate(block) {
+  console.log('Bio Cards block loaded!');
+  console.log('Block element:', block);
+  console.log('Children (cards):', block.children);
+}
